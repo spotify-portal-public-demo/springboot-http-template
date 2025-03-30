f901323f8bdb457aa389071acba705258a0804c3
Paul Frybarger <pfrybarger@spotify.com>
2025-03-30 21:29:12 +0100

    initial commit - template for springboot http

---
+++ skeleton/src/test/java/com/spotify/demo/${{values.java_package_name}}/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.spotify.demo.${{ values.java_package_name }};
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
